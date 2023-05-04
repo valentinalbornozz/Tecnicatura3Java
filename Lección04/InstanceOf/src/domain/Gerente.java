af114758ab9ce72a029c0d4efdf62649d457ef74
Valentin Albornoz <valeg486@gmail.com>
2023-05-04 15:42:14 -0300

    Clase 4
    Commiteo de clase 4 java: Sobreescritura, Polimorfismo y instanceof

---
+++ Lección04/InstanceOf/src/domain/Gerente.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class Gerente extends Empleado {
+    private String departamento;
+
+    public Gerente(String nombre, double sueldo, String departamente){
+        super(nombre, sueldo);
+        this.departamento = departamento;
+    }
+    //Sobreescribimos el método
+    @Override
+    public String obtenerDetalles(){
+        return super.obtenerDetalles()+", Departamento: "+this.departamento;
+    }
+    public String getDepartamento(){
+        return departamento;
+    }
+    public void setDepartamento(String departamento){
+        this.departamento = departamento;
+    }
+}
